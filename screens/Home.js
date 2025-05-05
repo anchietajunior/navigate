e838bce7df4a802ac82d039fff6db348fca32e30
José Anchieta <santosjr87@gmail.com>
2025-05-05 17:34:44 -0300

    Add README.md

--- screens/Home.js
+++ screens/Home.js
@@ -3,12 +3,12 @@ import { View, Text, Button } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
+    <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Página Home</Text>
       <Button
         title="Ir para Perfil"
         onPress={() => navigation.navigate('Profile')}
       />
-    </View>
+    </SafeAreaView>
   );
 }
--- screens/Profile.js
+++ screens/Profile.js
@@ -3,9 +3,9 @@ import { View, Text, Button } from 'react-native';
 
 export default function ProfileScreen({ navigation }) {
   return (
-    <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
+    <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Página Perfil</Text>
       <Button title="Voltar" onPress={() => navigation.goBack()} />
-    </View>
+    </SafeAreaView>
   );
 }
